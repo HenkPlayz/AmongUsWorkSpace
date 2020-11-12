16ac12b2a55ad28e04d51d6367dde21719c7f88d
HenkPlayz <User@Jogi198424>
2020-11-12 07:44:33 +0100

    created lamp

---
+++ src/main/java/net/mcreator/amongus/block/LampBlock.java
@@ -0,0 +1,49 @@
+
+package net.mcreator.amongus.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.amongus.AmongUsModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@AmongUsModElements.ModElement.Tag
+public class LampBlock extends AmongUsModElements.ModElement {
+	@ObjectHolder("among_us:lamp")
+	public static final Block block = null;
+	public LampBlock(AmongUsModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.GLASS).sound(SoundType.GLASS).hardnessAndResistance(1f, 10f).lightValue(0));
+			setRegistryName("lamp");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
